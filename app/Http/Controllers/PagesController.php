a9795a700102e5a8ee068bedf8328209e2263206
Adib Hanna <adibhanna@gmail.com>
2017-04-03 14:16:24 -0700

    add tags

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,21 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Blog;
+use Spatie\Tags\Tag;
 
+/**
+ * Class PagesController
+ * @package App\Http\Controllers
+ */
 class PagesController extends Controller
 {
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         return view('theme.index');
     }
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function blog()
     {
-        $posts = Blog::latest()->paginate();
-
-        return view('theme.blog', compact('posts'));
+        if($tag = request()->get('tag')) {
+            $posts = Blog::withAnyTags([$tag])->latest()->paginate();
+        } else {
+            $posts = Blog::latest()->paginate();
+        }
+        $tags = Tag::all();
+        return view('theme.blog', compact('posts', 'tags'));
     }
 
+    /**
+     * @param $slug
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function blogDetails($slug)
     {
         $post = Blog::whereSlug($slug)->firstOrFail();
--- resources/views/theme/blog.blade.php
+++ resources/views/theme/blog.blade.php
@@ -5,32 +5,32 @@
 </head>
 
 <body>
-	{{--<div id="preloader"></div>--}}
-    <div id="wrapper">
-       @include('theme.partials.header')
+{{--<div id="preloader"></div>--}}
+<div id="wrapper">
+@include('theme.partials.header')
 
-        <!-- subheader begin -->
-        <section id="subheader" data-speed="2" data-type="background">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
+<!-- subheader begin -->
+    <section id="subheader" data-speed="2" data-type="background">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
 
-                        <h1>Blog</h1>
-                    </div>
+                    <h1>Blog</h1>
                 </div>
             </div>
-        </section>
-        <!-- subheader close -->
+        </div>
+    </section>
+    <!-- subheader close -->
 
-        <div class="clearfix"></div>
+    <div class="clearfix"></div>
 
-        <!-- content begin -->
-        <div id="content">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8">
-                        <ul class="blog-list">
-                            @foreach($posts as $post)
+    <!-- content begin -->
+    <div id="content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <ul class="blog-list">
+                        @foreach($posts as $post)
                             <li>
                                 <div class="info">
                                     <div class="date-box">
@@ -40,9 +40,9 @@
                                 </div>
                                 <div class="preview">
                                     @if($post->photo != null)
-                                    <img src="/storage/{{ $post->photo }}" alt="" />
+                                        <img src="/storage/{{ $post->photo }}" alt=""/>
                                     @else
-                                    <img src="{{ asset('theme/img/blog/pic-blog-1.jpg') }}" alt="" />
+                                        <img src="{{ asset('theme/img/blog/pic-blog-1.jpg') }}" alt=""/>
                                     @endif
                                     <a href="/blog/{{ $post->slug }}">
                                         <h3 class="blog-title">{{ $post->title }}</h3>
@@ -53,72 +53,63 @@
                                     <a href="#">Matthew King</a>
                                     <span>|</span>
                                     @foreach($post->tags as $tag)
-                                        <a href="#">{{ $tag->name }}</a>
+                                        <a href="/blog?tag={{ $tag->slug }}">{{ $tag->name }}</a>
                                     @endforeach
                                 </div>
                             </li>
-                            @endforeach
-                        </ul>
+                        @endforeach
+                    </ul>
 
-                        <div class="clearfix"></div>
+                    <div class="clearfix"></div>
 
-                        <div class="text-center">
+                    <div class="text-center">
                         {{ $posts->links() }}
-                        </div>
-                    </div>
-
-                    <div id="sidebar" class="col-md-4">
-                        <!-- widget tags -->
-                        <div class="widget widget_tags">
-                            <h3><span>Tags</span></h3>
-                            <ul>
-                                <li><a href="#link">paralax</a></li>
-                                <li><a href="#link">carousel</a></li>
-                                <li><a href="#link">christian</a></li>
-                                <li><a href="#link">church</a></li>
-                                <li><a href="#link">clean</a></li>
-                                <li><a href="#link">flat</a></li>
-                                <li><a href="#link">revolution slider</a></li>
-                                <li><a href="#link">fundraising</a></li>
-                                <li><a href="#link">html 5</a></li>
-                                <li><a href="#link">ngo</a></li>
-                                <li><a href="#link">non profit</a></li>
-                                <li><a href="#link">religion</a></li>
-                            </ul>
-                        </div>
-
                     </div>
                 </div>
 
+                <div id="sidebar" class="col-md-4">
+                    <!-- widget tags -->
+                    <div class="widget widget_tags">
+                        <h3><span>Tags</span></h3>
+                        <ul>
+                            @foreach($tags as $tag)
+                                <li><a href="/blog?tag={{ $tag->slug }}">{{$tag->name}}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+
+                </div>
             </div>
+
         </div>
-        <!-- content close -->
-
-        <!-- footer begin -->
-        @include('theme.partials.footer')
-        <!-- footer close -->
     </div>
+    <!-- content close -->
+
+    <!-- footer begin -->
+@include('theme.partials.footer')
+<!-- footer close -->
+</div>
 
 
-    <script src="{{ asset('/theme/js/jquery.min.js') }}"></script>
-    <script src="{{ asset('/theme/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('/theme/js/jquery.isotope.min.js') }}"></script>
-    <script src="{{ asset('/theme/js/jquery.prettyPhoto.js') }}"></script>
-    <script src="{{ asset('/theme/js/easing.js') }}"></script>
-    <script src="{{ asset('/theme/js/jquery.ui.totop.js') }}"></script>
-    <script src="{{ asset('/theme/js/selectnav.js') }}"></script>
-    <script src="{{ asset('/theme/js/ender.js') }}"></script>
-    <script src="{{ asset('/theme/js/responsiveslides.min.js') }}"></script>
-    <script src="{{ asset('/theme/js/owl.carousel.js') }}"></script>
-    <script src="{{ asset('/theme/js/jquery.fitvids.js') }}"></script>
-    <script src="{{ asset('/theme/js/jquery.plugin.js') }}"></script>
-    <script src="{{ asset('/theme/js/jquery.countdown.js') }}"></script>
-    <script src="{{ asset('/theme/js/countdown-custom.js') }}"></script>
-    <script src="{{ asset('/theme/js/wow.min.js') }}"></script>
-    <script src="{{ asset('/theme/js/custom.js') }}"></script>
+<script src="{{ asset('/theme/js/jquery.min.js') }}"></script>
+<script src="{{ asset('/theme/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('/theme/js/jquery.isotope.min.js') }}"></script>
+<script src="{{ asset('/theme/js/jquery.prettyPhoto.js') }}"></script>
+<script src="{{ asset('/theme/js/easing.js') }}"></script>
+<script src="{{ asset('/theme/js/jquery.ui.totop.js') }}"></script>
+<script src="{{ asset('/theme/js/selectnav.js') }}"></script>
+<script src="{{ asset('/theme/js/ender.js') }}"></script>
+<script src="{{ asset('/theme/js/responsiveslides.min.js') }}"></script>
+<script src="{{ asset('/theme/js/owl.carousel.js') }}"></script>
+<script src="{{ asset('/theme/js/jquery.fitvids.js') }}"></script>
+<script src="{{ asset('/theme/js/jquery.plugin.js') }}"></script>
+<script src="{{ asset('/theme/js/jquery.countdown.js') }}"></script>
+<script src="{{ asset('/theme/js/countdown-custom.js') }}"></script>
+<script src="{{ asset('/theme/js/wow.min.js') }}"></script>
+<script src="{{ asset('/theme/js/custom.js') }}"></script>
 
-    <script src="{{ asset('/theme/rs-plugin/js/jquery.themepunch.plugins.min.js') }}"></script>
-    <script src="{{ asset('/theme/rs-plugin/js/jquery.themepunch.revolution.min.js') }}"></script>
+<script src="{{ asset('/theme/rs-plugin/js/jquery.themepunch.plugins.min.js') }}"></script>
+<script src="{{ asset('/theme/rs-plugin/js/jquery.themepunch.revolution.min.js') }}"></script>
 
 </body>
 </html>
