b7ec5232016e466216bd1ed84058da2f25998553
Adib Hanna <adibhanna@gmail.com>
2017-05-25 13:02:39 +0300

    quotes

--- resources/views/theme/partials/testimonies.blade.php
+++ resources/views/theme/partials/testimonies.blade.php
@@ -4,22 +4,21 @@
             <div class="col-md-8 col-md-offset-2">
                 <div id="testi-carousel" class="testi-slider text-center">
                     <div class="item">
-                        <img src="/theme/img/testi/pic%20(1).jpg" alt="" class="img-circle">
-                        <blockquote>Blessing theme has a real desire and heart for ministry within the local church</blockquote>
-                        <span class="testi-by">Aline Drummond</span>
+                        {{-- <img src="/theme/img/testi/pic%20(1).jpg" alt="" class="img-circle"> --}}
+                        <blockquote>“Rule with the heart of a servant. Serve with the heart of a king.”</blockquote>
+                        <span class="testi-by">Bill Johnson</span>
                     </div>
                     <div class="item">
-                        <img src="/theme/img/testi/pic%20(2).jpg" alt="" class="img-circle">
-                        <blockquote>I Just wanted to let you know how pleased we are and how great the Blessing theme is working for our National Church </blockquote>
-                        <span class="testi-by">Mortimer Elmo</span>
+                        {{-- <img src="/theme/img/testi/pic%20(2).jpg" alt="" class="img-circle"> --}}
+                        <blockquote>“I've read the last page of the Bible, it's all going to turn out all right.”</blockquote>
+                        <span class="testi-by">Billy Graham</span>
                     </div>
                     <div class="item">
-                        <img src="/theme/img/testi/pic%20(3).jpg" alt="" class="img-circle">
+                        {{-- <img src="/theme/img/testi/pic%20(3).jpg" alt="" class="img-circle"> --}}
                         <blockquote>
-                            Get in touch with Blessing theme today and get ready to see your church grow!
-
+                        “When wealth is lost, nothing is lost; when health is lost, something is lost; when character is lost, all is lost.”
                         </blockquote>
-                        <span class="testi-by">Marina Leopold</span>
+                        <span class="testi-by">Billy Graham</span>
                     </div>
 
                 </div>
