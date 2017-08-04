f7a21ea742fee9e932a781cfb07a2fd361dcc715
Adib Hanna <adibhanna@gmail.com>
2017-08-04 22:05:37 +0300

    change about me

--- resources/views/theme/partials/homepageabout.blade.php
+++ resources/views/theme/partials/homepageabout.blade.php
@@ -3,9 +3,9 @@
         <div class="row">
             <div class="col-md-6">
                 <h1>About me</h1>
-                <p>Matt is an Australian-born evangelist. He has been on staff at Bethel Church in California now for four years, with a focus in the Bethel Supernatural School of Ministry (BSSM). He has led numerous mission/ministry trips, most recently to Sweden, Uganda, Peru, India, and other parts of the United States. He has had the privilege of preaching the gospel of Jesus Christ to large audiences in those nations.</p>
-                <p>Matt is a teacher and trainer in evangelism at BSSM. He also has responsibility for the discipleship of approximately 60-70 BSSM students each year, as well as the mentorship of an intern team. He preaches and ministers unto the end of people making a decision to follow Christ, healing and miracles, transformational experiences of the Holy Spirit; and cities being saved.</p>
-                <p>Matt undertook his ministry training at the Bethel Supernatural School of Ministry at Bethel Church in Redding, California. He is also a graduate of the Reinhard Bonnke School of Evangelism. Matt is happily married to his wife, Elisabeth.</p>
+                <p>Matt is an Australian-born evangelist, commissioned by Bethel Church. He has spent the last four years on staff at Bethel Church in California, with a focus on leading and training the students in the school of ministry. Matt has led teams in the proclamation of the gospel and equipping of believers on several continents, partnering with local ministries. His most recent ministry trips have been to: Belgium, Sweden, the USA, Uganda, and Peru. He has had the wonderful privilege of preaching the gospel to audiences of considerable size in those nations. Matt takes delight in equipping and training the church in evangelism and a supernatural lifestyle. Overall, he ministers unto the breakthrough of the presence of God and seeing souls being saved, the manifestation of signs, wonders, and miracles; and cities being transformed.</p>
+                <p>Matt undertook his ministry training at the Bethel Supernatural School of Ministry at Bethel Church in Redding, California. He is also a graduate of the Reinhard Bonnke School of Evangelism.</p>
+                <p>Matt is happily married to his wife, Elisabeth.</p>
             </div>
 
             <div class="col-md-6">
