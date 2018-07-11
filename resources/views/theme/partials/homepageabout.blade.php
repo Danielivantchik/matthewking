42e973b6c5538ed40c29670f6e8963bf5cd432b7
Adib Hanna <adibhanna@gmail.com>
2018-07-11 22:33:11 +0300

    change about text

--- resources/views/theme/partials/homepageabout.blade.php
+++ resources/views/theme/partials/homepageabout.blade.php
@@ -3,9 +3,10 @@
         <div class="row">
             <div class="col-md-6">
                 <h1>About me</h1>
-                <p>Matt is an Australian-born evangelist, commissioned by Bethel Church. He has spent the last four years on staff at Bethel Church in California, with a focus on leading and training the students in the school of ministry. Matt has led teams in the proclamation of the gospel and equipping of believers on several continents, partnering with local ministries. His most recent ministry trips have been to: Belgium, Sweden, the USA, Uganda, and Peru. He has had the wonderful privilege of preaching the gospel to audiences of considerable size in those nations. Matt takes delight in equipping and training the church in evangelism and a supernatural lifestyle. Overall, he ministers unto the breakthrough of the presence of God and seeing souls being saved, the manifestation of signs, wonders, and miracles; and cities being transformed.</p>
-                <p>Matt undertook his ministry training at the Bethel Supernatural School of Ministry at Bethel Church in Redding, California. He is also a graduate of the Reinhard Bonnke School of Evangelism.</p>
-                <p>Matt is happily married to his wife, Elisabeth.</p>
+                <p>
+                    Matt is an Australian-born evangelist, commissioned by Bethel Church in California. He has recently ended four years on staff at Bethel Church, with a focus on leading and training the students in the school of ministry. He now ministers itinerantly under the church’s covering. Matt is the principal of: Impact - School of Revival - Europe, which is based in Norway. This full-time school is committed to developing passionate followers of Jesus who know and manifest His presence, power and purity. He is also a Revival Group Leader in the Bethel Leaders Network. Through this role he provides spiritual leadership for senior and associate church leaders. Matt has led teams in the proclamation of the gospel and equipping of believers on several continents, partnering with local ministries. He has had the wonderful privilege of preaching the gospel to audiences on numerous continents. He takes delight in equipping and training the church in evangelism and a lifestyle of signs and wonders.
+                </p>
+                <p>Matt is happily married to his wife, and they reside in Norway.</p>
             </div>
 
             <div class="col-md-6">
