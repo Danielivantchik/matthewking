ef8d40f1339ed9211dea29562386fc71b52c208d
Donovan Schaafsma <33253996+Ryukishi@users.noreply.github.com>
2023-07-07 23:23:39 +0200

    Create underconstruction.blade.php
    Added under construction page

---
+++ resources/views/theme/underconstruction.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <title>Matt King</title>
+  <style>
+        
+        * { font-family: verdana; COLOR: gray; font-size: 40px;}
+
+	</style>
+<head>
+</head>
+<body>
+  <br>
+  <br>
+  <br>
+  <br>
+	Under construction.
+  <br>
+  <br>
+</body>
+</html>
